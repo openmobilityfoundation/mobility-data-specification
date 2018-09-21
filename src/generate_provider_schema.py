6e5496c4d25624f3919d6adbae3dcab9239d752a
Ian Rose <ian.r.rose@gmail.com>
2018-09-20 18:46:48 -0700

    Add some docstrings.

--- src/generate_provider_schema.py
+++ src/generate_provider_schema.py
@@ -1,8 +1,17 @@
+"""
+generate_provider_schema.py
+
+When run, makes standalone schemas for the MDS provider API.
+"""
+
 import json
 import requests
 
 
 def get_point_schema():
+    """
+    Get the canonical schema for a GeoJSON point.
+    """
     p = requests.get("http://geojson.org/schema/Point.json")
     point = p.json()
     # Modify some metadata
@@ -11,6 +20,14 @@ def get_point_schema():
     return point
 
 def get_feature_collection_schema():
+    """
+    Get the canonical schema for a GeoJSON Feature Collection,
+    and make the following modifications to match the MDS spec:
+
+    1. Only allow GeoJSON Point features.
+    2. Those Point features *must* include a `timestamp` number property.
+    3. There must be *at least* two points in the FeatureCollection.
+    """
     # Get the canonical FeatureCollection schema
     fc = requests.get("http://geojson.org/schema/FeatureCollection.json")
     feature_collection = fc.json()
@@ -39,18 +56,22 @@ def get_feature_collection_schema():
             }
     return feature_collection
 
-# Get a local json file
 def get_json_file(path):
+    """
+    Load a JSON file from disk.
+    """
     with open(path) as f:
         data = json.load(f)
         return data
 
 
 if __name__ == '__main__':
+    # Load common data
     common = get_json_file('common.json')
     point = get_point_schema()
     feature_collection = get_feature_collection_schema()
 
+    # Create the standalone trips JSON schema by including the needed definitions
     trips = get_json_file('provider/trips.json')
     trips["definitions"] = {
             "Point": point,
@@ -61,10 +82,12 @@ if __name__ == '__main__':
             "version": common["definitions"]["version"],
             "uuid": common["definitions"]["uuid"],
             }
+    # Write to the `provider` directory.
     with open("../provider/trips.json", "w") as tripfile:
         tripfile.write(json.dumps(trips, indent=2))
 
 
+    # Create the standalone status_changes JSON schema by including the needed definitions
     status_changes = get_json_file('provider/status_changes.json')
     status_changes["definitions"] = {
             "Point": point,
@@ -74,6 +97,6 @@ if __name__ == '__main__':
             "version": common["definitions"]["version"],
             "uuid": common["definitions"]["uuid"],
             }
-
+    # Write to the `provider` directory.
     with open("../provider/status_changes.json", "w") as statusfile:
         statusfile.write(json.dumps(status_changes, indent=2))
