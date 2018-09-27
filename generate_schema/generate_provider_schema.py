e4ad36c320fa03e71abf52666df062638d3c5d16
Kegan Maher <kegan.maher@smgov.net>
2018-09-27 15:25:57 -0700

    more specific definition  for our Feature/Feature Collection overloads
    generalized the generator code a bit to handle other situations

--- generate_schema/generate_provider_schema.py
+++ generate_schema/generate_provider_schema.py
@@ -7,6 +7,12 @@ When run, makes standalone schemas for the MDS provider API.
 import json
 import requests
 
+POINT = "Point"
+MDS_FEATURE_POINT = "MDS_Feature_Point"
+MDS_FEATURECOLLECTION_ROUTE = "MDS_FeatureCollection_Route"
+
+def get_definition(id):
+    return "#/definitions/{}".format(id)
 
 def get_point_schema():
     """
@@ -16,61 +22,64 @@ def get_point_schema():
     point = p.json()
     # Modify some metadata
     point.pop("$schema")
-    point["$id"] = "#/definitions/Point"
+    point["$id"] = get_definition(POINT)
     return point
 
-def get_feature_schema():
+def get_feature_schema(id=None, title=None, geometry=None, properties=None, required=None):
     """
     Get the canonical schema for a GeoJSON Feature,
-    and make the following modifications to match the MDS spec:
+    and make any given modifications.
 
-    1. Only allow GeoJSON Point features.
-    2. Those Point features *must* include a `timestamp` number property.
+    :id: overrides the `$id` metadata
+    :title: overrides the `title` metadata
+    :geometry: overrides the allowed `geometry` for the Feature
+    :properties: overrides the `properties` definitions for this Feature
+    :required: is a list of required :properties:
     """
     # Get the canonical Feature schema
     f = requests.get("http://geojson.org/schema/Feature.json")
     feature = f.json()
+
     # Modify some metadata
     feature.pop("$schema")
-    feature["$id"] = "#/definitions/FeatureMDS"
-    feature["title"] = "GeoJSON Feature (MDS spin)"
+    if id is not None:
+        feature["$id"] = id
+    if title is not None:
+        feature["title"] = title
 
-    # Only accept Point geometry in the Feature
-    feature["properties"]["geometry"] = { "$ref": "#/definitions/Point" }
+    if geometry is not None:
+        feature["properties"]["geometry"] = geometry
 
-     # Add a required timestamp property
-    properties = feature["properties"]["properties"]
-    properties["required"] = ["timestamp"]
-    properties["properties"] = {
-            "timestamp": {
-                "type": "number",
-                "minimum": 0.0
-                }
-            }
+    f_properties = feature["properties"]["properties"]
+    if required is not None:
+        f_properties["required"] = required
+    if properties is not None:
+        f_properties["properties"] = properties
 
     return feature
 
-def get_feature_collection_schema():
+def get_feature_collection_schema(id=None, title=None, features=None):
     """
     Get the canonical schema for a GeoJSON Feature Collection,
-    and make the following modifications to match the MDS spec:
+    and make any given modifications.
 
-    1. Only allow MDS Features.
-    2. There must be *at least* two Features in the FeatureCollection.
+    :id: overrides the `$id` metadata
+    :title: overrides the `title` metadata
+    :features: overrides the allowed `features` for the FeatureCollection
     """
     # Get the canonical FeatureCollection schema
     fc = requests.get("http://geojson.org/schema/FeatureCollection.json")
     feature_collection = fc.json()
     # Modify some metadata
     feature_collection.pop("$schema")
-    feature_collection["$id"] = "#/definitions/FeatureCollectionMDS"
-    feature_collection["title"] = "GeoJSON FeatureCollection (MDS spin)"
+    if id is not None:
+        feature_collection["$id"] = id
+    if title is not None:
+        feature_collection["title"] = title
 
-    # Only accept the MDS-specific Feature in the collection
-    feature_collection["properties"]["features"]["items"] = { "$ref": "#/definitions/FeatureMDS" }
-
-    # Force the FeatureCollection to at least have a start and an end.
-    feature_collection["properties"]["features"]["minItems"] = 2
+    if features is not None:
+        fc_features = feature_collection["properties"]["features"]
+        feature_collection["properties"]["features"] = { **fc_features, **features }
 
     return feature_collection
 
@@ -87,15 +96,31 @@ if __name__ == '__main__':
     # Load common data
     common = get_json_file('common.json')
     point = get_point_schema()
-    feature = get_feature_schema()
-    feature_collection = get_feature_collection_schema()
+    # Craft the MDS specific types
+    mds_feature_point = get_feature_schema(
+        id = get_definition(MDS_FEATURE_POINT),
+        title = "MDS GeoJSON Feature Point",
+        # Only allow GeoJSON Point feature geometry
+        geometry = { "$ref": get_definition(POINT) },
+        # Point features *must* include a `timestamp` property.
+        properties = { "timestamp": { "$ref": get_definition("timestamp") } },
+        required = ["timestamp"]
+    )
+    mds_feature_collection_route = get_feature_collection_schema(
+        id = get_definition(MDS_FEATURECOLLECTION_ROUTE),
+        title = "MDS GeoJSON FeatureCollection Route",
+        # 1. Only allow MDS Feature Points
+        # 2. There must be *at least* two Features in the FeatureCollection.
+        features = { "items": { "$ref": get_definition(MDS_FEATURE_POINT) }, "minItems": 2 }
+    )
+
 
     # Create the standalone trips JSON schema by including the needed definitions
     trips = get_json_file('provider/trips.json')
     trips["definitions"] = {
-            "Point": point,
-            "FeatureMDS": feature,
-            "FeatureCollectionMDS": feature_collection,
+            POINT: point,
+            MDS_FEATURE_POINT: mds_feature_point,
+            MDS_FEATURECOLLECTION_ROUTE: mds_feature_collection_route,
             "links": common["definitions"]["links"],
             "propulsion_type": common["definitions"]["propulsion_type"],
             "vehicle_type": common["definitions"]["vehicle_type"],
@@ -110,8 +135,8 @@ if __name__ == '__main__':
     # Create the standalone status_changes JSON schema by including the needed definitions
     status_changes = get_json_file('provider/status_changes.json')
     status_changes["definitions"] = {
-            "Point": point,
-            "FeatureMDS": feature,
+            POINT: point,
+            MDS_FEATURE_POINT: mds_feature_point,
             "links": common["definitions"]["links"],
             "propulsion_type": common["definitions"]["propulsion_type"],
             "vehicle_type": common["definitions"]["vehicle_type"],
