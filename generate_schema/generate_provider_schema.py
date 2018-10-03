4609e7866ac981fa5a2f2838721bf1a02f640fa0
Ian Rose <ian.r.rose@gmail.com>
2018-10-02 18:26:30 -0600

    Make `event_type_reason` conditional on `event_type`. (#103)
    * Make `event_type_reason` conditional on `event_type`.
    
    * Relax uuid format.

--- generate_schema/generate_provider_schema.py
+++ generate_schema/generate_provider_schema.py
@@ -5,6 +5,7 @@ When run, makes standalone schemas for the MDS provider API.
 """
 
 import json
+import jsonschema
 import requests
 
 POINT = "Point"
@@ -128,6 +129,9 @@ if __name__ == '__main__':
             "version": common["definitions"]["version"],
             "uuid": common["definitions"]["uuid"],
             }
+
+    # Check that it is a valid schema
+    jsonschema.Draft6Validator.check_schema(trips)
     # Write to the `provider` directory.
     with open("../provider/trips.json", "w") as tripfile:
         tripfile.write(json.dumps(trips, indent=2))
@@ -145,6 +149,9 @@ if __name__ == '__main__':
             "version": common["definitions"]["version"],
             "uuid": common["definitions"]["uuid"],
             }
+
+    # Check that it is a valid schema
+    jsonschema.Draft6Validator.check_schema(status_changes)
     # Write to the `provider` directory.
     with open("../provider/status_changes.json", "w") as statusfile:
         statusfile.write(json.dumps(status_changes, indent=2))
