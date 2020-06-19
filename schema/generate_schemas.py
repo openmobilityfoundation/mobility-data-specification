748efb6a9b9bc0e05acbb80a851726a020996678
Kegan Maher <kegan.maher@gmail.com>
2020-06-18 19:07:03 -0700

    deduplicate Provider schema generation
    This is a refactor of the Provider schema templates and generation
    process to make more reuse of common types and structures:
    
    * The repeated structure from outside wrapper has been refactored into
      templates/provider/endpoint.json
    
    * Common vehicle information (provider_id, vehicle_type, etc.) has been
      refactored into a defintion in templates/common.json, which is added
      to the list of definitions for each endpoint
    
    * Each endpoint specific template has been refactored to contain only
      those properties and definitions specific to the endpoint; common
      properties and definitions are merged at generation time
    
    * The generation script was improved for readability and runtime feedback
    
    * The generation script accepts arguments for selectively generating
      Agency and/or Provider schemas (default is still to generate both)

--- schema/generate_schemas.py
+++ schema/generate_schemas.py
@@ -1,30 +1,45 @@
 """
-generate_provider_schema.py
+Generate the JSON Schema documents for MDS endpoints.
 
-When run, makes standalone schemas for the MDS provider API.
+USAGE:
+    python generate_schemas.py [--agency] [--provider]
 """
 
 import json
 import jsonschema
 import requests
+import sys
+
 
 POINT = "Point"
-MULTIPOLYGON = "MultiPolygon"
 MDS_FEATURE_POINT = "MDS_Feature_Point"
 MDS_FEATURECOLLECTION_ROUTE = "MDS_FeatureCollection_Route"
 
-def get_definition(id):
+
+def load_json(path):
+    """
+    Load a JSON file from disk.
+    """
+    with open(path) as f:
+        data = json.load(f)
+        return data
+
+
+def get_definition_id(id):
+    """
+    Generate a JSON Schema definition reference for the given id.
+    """
     return f"#/definitions/{id}"
+
 
 def get_point_schema():
     """
     Get the canonical schema for a GeoJSON point.
     """
-    p = requests.get("http://geojson.org/schema/Point.json")
-    point = p.json()
+    point = requests.get("http://geojson.org/schema/Point.json").json()
     # Modify some metadata
     point.pop("$schema")
-    point["$id"] = get_definition(POINT)
+    point["$id"] = get_definition_id(POINT)
     # enforce lat/lon bounds
     point["properties"]["coordinates"]["maxItems"] = 2
     point["properties"]["coordinates"]["items"] = [
@@ -41,16 +56,6 @@ def get_point_schema():
     ]
     return point
 
-def get_multipolygon_schema():
-    """
-    Get the canonical schema for a GeoJSON MultiPolygon.
-    """
-    mp = requests.get("http://geojson.org/schema/MultiPolygon.json")
-    multipolygon = mp.json()
-    # Modify some metadata
-    multipolygon.pop("$schema")
-    multipolygon["$id"] = get_definition(MULTIPOLYGON)
-    return multipolygon
 
 def get_feature_schema(id=None, title=None, geometry=None, properties=None, required=None):
     """
@@ -64,8 +69,7 @@ def get_feature_schema(id=None, title=None, geometry=None, properties=None, requ
     :required: is a list of required :properties:
     """
     # Get the canonical Feature schema
-    f = requests.get("http://geojson.org/schema/Feature.json")
-    feature = f.json()
+    feature = requests.get("http://geojson.org/schema/Feature.json").json()
 
     # Modify some metadata
     feature.pop("$schema")
@@ -87,6 +91,7 @@ def get_feature_schema(id=None, title=None, geometry=None, properties=None, requ
 
     return feature
 
+
 def get_feature_collection_schema(id=None, title=None, features=None):
     """
     Get the canonical schema for a GeoJSON Feature Collection,
@@ -97,8 +102,7 @@ def get_feature_collection_schema(id=None, title=None, features=None):
     :features: overrides the allowed `features` for the FeatureCollection
     """
     # Get the canonical FeatureCollection schema
-    fc = requests.get("http://geojson.org/schema/FeatureCollection.json")
-    feature_collection = fc.json()
+    feature_collection = requests.get("http://geojson.org/schema/FeatureCollection.json").json()
     # Modify some metadata
     feature_collection.pop("$schema")
     if id is not None:
@@ -112,179 +116,273 @@ def get_feature_collection_schema(id=None, title=None, features=None):
 
     return feature_collection
 
-def get_json_file(path):
+
+def get_property_definition(id, common_definitions, ref=""):
     """
-    Load a JSON file from disk.
+    Return a tuple (property, definition) of schema elements for the given id.
     """
-    with open(path) as f:
-        data = json.load(f)
-        return data
+    # property ref definition
+    definition = { id: common_definitions.get(id) }
+    # the property
+    ref = ref or get_definition_id(id)
+    prop = { id: { "$id": f"#/properties/{id}", "$ref": ref } }
+
+    return prop, definition
 
 
-if __name__ == '__main__':
-    # Load common data
-    common = get_json_file('./templates/common.json')
+def create_provider_schema(endpoint, common_definitions, extra_definitions={}):
+    """
+    Generate the Provider payload schema for the given endpoint.
+    """
+    schema = load_json("./templates/provider/endpoint.json")
+    schema["$id"] = schema["$id"].replace("endpoint.json", f"{endpoint}.json")
+    schema["title"] = schema["title"].replace("endpoint", endpoint)
+
+    # MDS specific geography
     point = get_point_schema()
-    multipolygon = get_multipolygon_schema()
-    # Craft the MDS specific types
     mds_feature_point = get_feature_schema(
-        id = get_definition(MDS_FEATURE_POINT),
+        id = get_definition_id(MDS_FEATURE_POINT),
         title = "MDS GeoJSON Feature Point",
         # Only allow GeoJSON Point feature geometry
-        geometry = { "$ref": get_definition(POINT) },
+        geometry = { "$ref": get_definition_id(POINT) },
         # Point features *must* include a `timestamp` property.
-        properties = { "timestamp": { "$ref": get_definition("timestamp") } },
+        properties = { "timestamp": { "$ref": get_definition_id("timestamp") } },
         required = ["timestamp"]
     )
+
+    # merge custom definitions with relevant common definitions
+    definitions = {
+        POINT: point,
+        MDS_FEATURE_POINT: mds_feature_point,
+        "propulsion_type": common_definitions["propulsion_type"],
+        "string": common_definitions["string"],
+        "timestamp": common_definitions["timestamp"],
+        "uuid": common_definitions["uuid"],
+        "vehicle_type": common_definitions["vehicle_type"],
+        "version": common_definitions["version"]
+    }
+    definitions.update(extra_definitions)
+
+    # insert definitions into schema
+    schema["definitions"].update(definitions)
+
+    # merge endpoint-specific schema with standard vehicle info
+    endpoint_schema = load_json(f"./templates/provider/{endpoint}.json")
+    items = endpoint_schema[endpoint]["items"]
+    vehicle_schema = common_definitions["vehicle"]
+    items["required"] = vehicle_schema["required"] + items["required"]
+    items["properties"] = { **vehicle_schema["properties"], **items["properties"] }
+
+    # merge this endpoint-specific schema into the endpoint template
+    data_schema = schema["properties"]["data"]
+    data_schema["required"] = [endpoint]
+    data_schema["properties"] = endpoint_schema
+
+    return schema
+
+
+def create_trips_schema(common_definitions):
+    """
+    Create the schema for the /trips endpoint.
+    """
+    # generate the route definition
     mds_feature_collection_route = get_feature_collection_schema(
-        id = get_definition(MDS_FEATURECOLLECTION_ROUTE),
+        id = get_definition_id(MDS_FEATURECOLLECTION_ROUTE),
         title = "MDS GeoJSON FeatureCollection Route",
         # 1. Only allow MDS Feature Points
         # 2. There must be *at least* two Features in the FeatureCollection.
-        features = { "items": { "$ref": get_definition(MDS_FEATURE_POINT) }, "minItems": 2 }
+        features = { "items": { "$ref": get_definition_id(MDS_FEATURE_POINT) }, "minItems": 2 }
     )
+    trips_definitions = { MDS_FEATURECOLLECTION_ROUTE: mds_feature_collection_route }
 
-    # Provider Schemas #
+    # create the trips schema
+    schema = create_provider_schema("trips", common_definitions, trips_definitions)
 
-    ## /trips Schema ##
+    # verify schema validity
+    jsonschema.Draft6Validator.check_schema(schema)
 
-    # Create the standalone trips JSON schema by including the needed definitions
-    trips = get_json_file('./templates/provider/trips.json')
-    trips["definitions"] = {
-        POINT: point,
-        MDS_FEATURE_POINT: mds_feature_point,
-        MDS_FEATURECOLLECTION_ROUTE: mds_feature_collection_route,
-        "links": common["definitions"]["links"],
-        "propulsion_type": common["definitions"]["propulsion_type"],
-        "timestamp": common["definitions"]["timestamp"],
-        "vehicle_type": common["definitions"]["vehicle_type"],
-        "version": common["definitions"]["version"],
-        "uuid": common["definitions"]["uuid"],
-    }
-    # Check that it is a valid schema
-    jsonschema.Draft6Validator.check_schema(trips)
-    # Write to the `provider` directory.
-    with open("../provider/dockless/trips.json", "w") as tripfile:
-        tripfile.write(json.dumps(trips, indent=2))
+    return schema
 
-    ## /status_changes Schema ##
 
-    # Create the standalone status_changes JSON schema by including the needed definitions
-    status_changes = get_json_file('./templates/provider/status_changes.json')
-    status_changes["definitions"] = {
-        POINT: point,
-        MDS_FEATURE_POINT: mds_feature_point,
-        "links": common["definitions"]["links"],
-        "propulsion_type": common["definitions"]["propulsion_type"],
-        "timestamp": common["definitions"]["timestamp"],
-        "vehicle_type": common["definitions"]["vehicle_type"],
-        "version": common["definitions"]["version"],
-        "uuid": common["definitions"]["uuid"],
-    }
-    # Check that it is a valid schema
-    jsonschema.Draft6Validator.check_schema(status_changes)
-    # Write to the `provider` directory.
-    with open("../provider/dockless/status_changes.json", "w") as statusfile:
-        statusfile.write(json.dumps(status_changes, indent=2))
+def create_status_changes_schema(common_definitions):
+    """
+    Create the schema for the /status_changes endpoint.
+    """
+    schema = create_provider_schema("status_changes", common_definitions)
 
-    ## /events Schema ##
+    # verify schema validity
+    jsonschema.Draft6Validator.check_schema(schema)
 
-    # /events is the same as status_changes, but allows paging
-    events = dict(status_changes)
-    events["$id"] = events["$id"].replace("status_changes", "events")
-    events["title"] = "The MDS Provider Schema, events payload"
-    events["definitions"] = {
-        "links": common["definitions"]["links"]
-    }
-    events["properties"]["links"] = {
-        "$id": "#/properties/links",
-        "$ref": "#/definitions/links"
-    }
-    # Check that it is a valid schema
-    jsonschema.Draft6Validator.check_schema(events)
-    # Write to the `provider` directory.
-    with open("../provider/dockless/events.json", "w") as eventsfile:
-        eventsfile.write(json.dumps(events, indent=2))
+    return schema
 
-    ## /vehicles Schema ##
 
-    # Create the standalone vehicles JSON schema by including the needed definitions
-    vehicles = get_json_file('./templates/provider/vehicles.json')
-    vehicles["definitions"] = {
-        POINT: point,
-        MDS_FEATURE_POINT: mds_feature_point,
-        "links": common["definitions"]["links"],
-        "propulsion_type": common["definitions"]["propulsion_type"],
-        "timestamp": common["definitions"]["timestamp"],
-        "vehicle_type": common["definitions"]["vehicle_type"],
-        "version": common["definitions"]["version"],
-        "uuid": common["definitions"]["uuid"],
-    }
-    # Check that it is a valid schema
-    jsonschema.Draft6Validator.check_schema(vehicles)
-    # Write to the `provider` directory.
-    with open("../provider/dockless/vehicles.json", "w") as vehiclesfile:
-        vehiclesfile.write(json.dumps(vehicles, indent=2))
+def create_events_schema(common_definitions):
+    """
+    Create the schema for the /events endpoint.
+    """
+    links_prop, links_def = get_property_definition("links", common_definitions)
 
-    # Agency Schemas #
+    # events is the same as status_changes, but allows paging
+    schema = create_provider_schema("status_changes", common_definitions, links_def)
+    schema["$id"] = schema["$id"].replace("status_changes", "events")
+    schema["title"] = schema["title"].replace("status_changes", "events")
+    schema["properties"].update(links_prop)
+
+    # verify schema validity
+    jsonschema.Draft6Validator.check_schema(schema)
+
+    return schema
+
+
+def create_vehicles_schema(common_definitions):
+    """
+    Create the schema for the /vehicles endpoint.
+    """
+    definitions, properties = {}, {}
+
+    prop, defn = get_property_definition("links", common_definitions)
+    definitions.update(defn)
+    properties.update(prop)
+
+    prop, _ = get_property_definition("last_updated", common_definitions, ref=get_definition_id("timestamp"))
+    properties.update(prop)
+
+    prop, defn = get_property_definition("ttl", common_definitions)
+    definitions.update(defn)
+    properties.update(prop)
+
+    schema = create_provider_schema("vehicles", common_definitions, definitions)
+
+    # update list of required and properties object
+    schema["required"].extend(["last_updated", "ttl"])
+    schema["properties"].update(properties)
+
+    # verify schema validity
+    jsonschema.Draft6Validator.check_schema(schema)
+
+    return schema
+
+
+def write_provider_schemas(common_definitions):
+    """
+    Create each of the Provider endpoint schema files in the target directory
+    """
+    print("\nStarting to generate Provider JSON Schemas...\n")
+
+    # /trips
+    trips = create_trips_schema(common_definitions)
+    with open("../provider/dockless/trips.json", "w") as schemafile:
+        schemafile.write(json.dumps(trips, indent=2))
+        print("Wrote trips.json")
+
+    # /status_changes
+    status_changes = create_status_changes_schema(common_definitions)
+    with open("../provider/dockless/status_changes.json", "w") as schemafile:
+        schemafile.write(json.dumps(status_changes, indent=2))
+        print("Wrote status_changes.json")
+
+    # /events
+    events = create_events_schema(common_definitions)
+    with open("../provider/dockless/events.json", "w") as schemafile:
+        schemafile.write(json.dumps(events, indent=2))
+        print("Wrote events.json")
+
+    # /vehicles
+    vehicles = create_vehicles_schema(common_definitions)
+    with open("../provider/dockless/vehicles.json", "w") as schemafile:
+        schemafile.write(json.dumps(vehicles, indent=2))
+        print("Wrote vehicles.json")
+
+    print("\nFinished generating Provider JSON Schemas")
+
+
+def write_agency_schemas(common_definitions):
+    """
+    Create the Agency schemas in the target directory.
+    """
+    print("\nStarting to generate Agency JSON Schemas...\n")
 
     ## GET VEHICLE ##
 
     # Create the standalone GET vehicle JSON schema by including the needed definitions
-    get_vehicle = get_json_file('./templates/agency/get_vehicle.json')
+    get_vehicle = load_json('./templates/agency/get_vehicle.json')
     get_vehicle["definitions"] = {
-        "propulsion_type": common["definitions"]["propulsion_type"],
-        "vehicle_type": common["definitions"]["vehicle_type"],
-        "vehicle_status": common["definitions"]["vehicle_status"],
-        "vehicle_event": common["definitions"]["vehicle_event"],
-        "timestamp": common["definitions"]["timestamp"],
-        "uuid": common["definitions"]["uuid"],
+        "propulsion_type": common_definitions["propulsion_type"],
+        "vehicle_type": common_definitions["vehicle_type"],
+        "vehicle_status": common_definitions["vehicle_status"],
+        "vehicle_event": common_definitions["vehicle_event"],
+        "timestamp": common_definitions["timestamp"],
+        "uuid": common_definitions["uuid"],
     }
     # Check that it is a valid schema
     jsonschema.Draft6Validator.check_schema(get_vehicle)
     # Write to the `agency` directory.
     with open("../agency/get_vehicle.json", "w") as file:
         file.write(json.dumps(get_vehicle, indent=2))
+        print("Wrote get_vehicle.json")
 
     ## POST VEHICLE ##
 
     # Create the standalone POST vehicle JSON schema by including the needed definitions
-    post_vehicle = get_json_file('./templates/agency/post_vehicle.json')
+    post_vehicle = load_json('./templates/agency/post_vehicle.json')
     post_vehicle["definitions"] = {
-        "propulsion_type": common["definitions"]["propulsion_type"],
-        "vehicle_type": common["definitions"]["vehicle_type"],
-        "uuid": common["definitions"]["uuid"],
+        "propulsion_type": common_definitions["propulsion_type"],
+        "vehicle_type": common_definitions["vehicle_type"],
+        "uuid": common_definitions["uuid"],
     }
     # Check that it is a valid schema
     jsonschema.Draft6Validator.check_schema(post_vehicle)
     # Write to the `agency` directory.
     with open("../agency/post_vehicle.json", "w") as file:
         file.write(json.dumps(post_vehicle, indent=2))
+        print("Wrote post_vehicle.json")
 
     ## POST VEHICLE EVENT ##
 
     # Create the standalone POST vehicle event JSON schema by including the needed definitions
-    post_vehicle_event = get_json_file('./templates/agency/post_vehicle_event.json')
+    post_vehicle_event = load_json('./templates/agency/post_vehicle_event.json')
     post_vehicle_event["definitions"] = {
-        "vehicle_event": common["definitions"]["vehicle_event"],
-        "telemetry": common["definitions"]["telemetry"],
-        "uuid": common["definitions"]["uuid"],
+        "vehicle_event": common_definitions["vehicle_event"],
+        "telemetry": common_definitions["telemetry"],
+        "uuid": common_definitions["uuid"],
     }
     # Check that it is a valid schema
     jsonschema.Draft6Validator.check_schema(post_vehicle_event)
     # Write to the `agency` directory.
     with open("../agency/post_vehicle_event.json", "w") as file:
         file.write(json.dumps(post_vehicle_event, indent=2))
+        print("Wrote post_vehicle_event.json")
 
     ## POST VEHICLE TELEMETRY ##
 
     # Create the standalone POST vehicle telemetry JSON schema by including the needed definitions
-    post_vehicle_telemetry = get_json_file('./templates/agency/post_vehicle_telemetry.json')
+    post_vehicle_telemetry = load_json('./templates/agency/post_vehicle_telemetry.json')
     post_vehicle_telemetry["definitions"] = {
-        "telemetry": common["definitions"]["telemetry"],
+        "telemetry": common_definitions["telemetry"],
     }
     # Check that it is a valid schema
     jsonschema.Draft6Validator.check_schema(post_vehicle_telemetry)
     # Write to the `agency` directory.
     with open("../agency/post_vehicle_telemetry.json", "w") as file:
         file.write(json.dumps(post_vehicle_telemetry, indent=2))
+        print("Wrote post_vehicle_telemetry.json")
+
+    print("\nFinished generating Agency JSON Schemas")
+
+
+if __name__ == "__main__":
+    common = load_json("./templates/common.json")
+    common_definitions = common["definitions"]
+
+    if len(sys.argv) == 1:
+        write_agency_schemas(common_definitions)
+        write_provider_schemas(common_definitions)
+    else:
+        if "--agency" in sys.argv:
+            write_agency_schemas(common_definitions)
+            sys.argv.remove("--agency")
+        if "--provider" in sys.argv:
+            write_provider_schemas(common_definitions)
+            sys.argv.remove("--provider")
+        if len(sys.argv) > 1:
+            print(__doc__)
